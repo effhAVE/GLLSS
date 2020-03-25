522fc62e96bc0a449d70c1319b7a0cd917f5bd73
Grzegorz K <42100427+effhAVE@users.noreply.github.com>
2020-03-25 01:52:40 +0100

    Fix for router history mode

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const winston = require("winston");
 const express = require("express");
 const app = express();
+const history = require("connect-history-api-fallback");
 
+app.use(history());
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
