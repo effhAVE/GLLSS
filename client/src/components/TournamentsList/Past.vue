f592b8ce41c6a14fb0806edb7d6336a6f6d292fd
Grzegorz K <42100427+effhAVE@users.noreply.github.com>
2020-05-16 20:13:11 +0200

    Fixed a typo in previous fix.

--- client/src/components/TournamentsList/Past.vue
+++ client/src/components/TournamentsList/Past.vue
@@ -145,7 +145,7 @@ export default {
       return round.teamLeads.some(TLObj => TLObj.host === this.user._id);
     },
     isHostingTournament(rounds) {
-      return rounds.some(round => round.isHosting);
+      return rounds.some(round => round.isHosting || round.isLeading);
     },
     onAvailabilityChange(value, tournament, round) {
       this.$emit("availabilityChange", { value, tournament, round });
