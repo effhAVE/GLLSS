f5e61a1da370f299905a955b1bc0c3cde218cc45
Grzegorz K <42100427+effhAVE@users.noreply.github.com>
2020-05-16 20:05:45 +0200

    Past tournaments hosted checkbox fix.

--- client/src/components/TournamentsList/Past.vue
+++ client/src/components/TournamentsList/Past.vue
@@ -145,7 +145,7 @@ export default {
       return round.teamLeads.some(TLObj => TLObj.host === this.user._id);
     },
     isHostingTournament(rounds) {
-      return rounds.some(round => round.isHosting);
+      return rounds.some(round => round.isHosting || rounds.isLeading);
     },
     onAvailabilityChange(value, tournament, round) {
       this.$emit("availabilityChange", { value, tournament, round });
