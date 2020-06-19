6d6aec207dab2857f40a5024f01936c64de13ebc
Grzegorz K <42100427+effhAVE@users.noreply.github.com>
2020-06-19 17:58:14 +0200

    Changed formatting for articles, addes table of content. CTRL + S saves the article when editing.

--- client/src/components/Forms/ArticleForm.vue
+++ client/src/components/Forms/ArticleForm.vue
@@ -4,16 +4,8 @@
       <v-form ref="form" style="min-width: 500px" v-model="valid">
         <v-text-field v-model="draft.title" color="accent" label="Title" :rules="validations.required"></v-text-field>
         <v-text-field v-model="draft.previewImageURL" color="accent" label="Preview image URL" :rules="validations.url"></v-text-field>
-        <v-textarea v-model="draft.contentShort" label="Preview text" color="accent" outlined row-height="18" auto-grow></v-textarea>
-        <v-textarea
-          v-model="draft.content"
-          label="Content"
-          color="accent"
-          outlined
-          row-height="40"
-          auto-grow
-          :rules="validations.required"
-        ></v-textarea>
+        <v-textarea v-model="draft.contentShort" label="Preview text" color="accent" outlined dense rows="4"></v-textarea>
+        <v-textarea v-model="draft.content" label="Content" color="accent" outlined :rules="validations.required" rows="12"></v-textarea>
         <v-row>
           <v-spacer></v-spacer>
           <v-btn color="accent black--text" class="mt-8" text @click="$emit('submit', draft)" :disabled="!valid">
@@ -50,7 +42,26 @@ export default {
   mounted() {
     if (this.article) {
       this.draft = Object.assign({}, this.article);
+
+      this._keyListener = function(event) {
+        if (event.key === "s" && (event.ctrlKey || event.metaKey)) {
+          event.preventDefault();
+          if (this.valid) {
+            this.$emit("submit", this.draft);
+          } else {
+            this.$store.commit("snackbarMessage", {
+              message: "Article is not valid.",
+              type: "error"
+            });
+          }
+        }
+      };
+
+      document.addEventListener("keydown", this._keyListener.bind(this));
     }
+  },
+  beforeDestroy() {
+    document.removeEventListener("keydown", this._keyListener);
   }
 };
 </script>
